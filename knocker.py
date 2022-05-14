4bf823c80c388920528e76c8c5d7b8f36e17c730
Ryan Eakin <eakinryan@hotmail.com>
2022-05-14 15:16:05 -0700

    added new prototype for testing

--- knocker.py
+++ knocker.py
@@ -2,10 +2,14 @@
 import os
 import sys
 import scapy.all as scapy
+import time
 
 def main():
+    ports=[1000,2000,3000,4000,1003]
     #send a UDP packet to port 1000
-    scapy.send(scapy.IP(dst="127.0.0.1")/scapy.UDP(dport=4000))
+    for port in ports:
+        scapy.sendp(scapy.Ether(dst="34:C9:3D:23:12:D4")/scapy.IP(dst="192.168.1.83")/scapy.UDP(dport=port))
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
---
+++ testing.py
@@ -0,0 +1,44 @@
+#port knocking server using watchdog and scapy
+#!/usr/bin/env python3
+
+import os
+import sys
+import scapy.all as scapy
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+import datetime
+
+class MyHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.last_mod = datetime.datetime.now()
+
+    def on_modified(self, event):
+        if datetime.datetime.now() - self.last_mod < datetime.timedelta(seconds=1):
+            return
+        else:
+            self.last_mod = datetime.datetime.now()
+
+        print(event.event_type)
+        if event.event_type == 'modified':
+            print("File modified")
+
+
+def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path='/var/log/firewall-droppd.log', recursive=False)
+    observer.start()
+    #if the file is modified, send a UDP packet to port 1000
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+if __name__ == "__main__":
+    main()
